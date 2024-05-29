4bea7b3bec1ee7727d8105a28c2ee1be9a41876d
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-05-29 13:46:21 +0900

    commit

---
+++ src/test/java/com/usopen/bankbackend/UsopenBankBackend20240529ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usopen.bankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsopenBankBackend20240529ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
